b5ae6d474d4373a4000e15667f8d889ddf755a4d
Mac Thurston <thurston@caltech.edu>
2024-01-12 14:42:17 -0800

    updating grasp+object analysis pipeline
    trying to get NoGo analysis to work

--- call_spikesorter.m
+++ call_spikesorter.m
@@ -15,7 +15,7 @@ subject = hst.Subject(subject_id);
 % Blackrock.thresholdSession('20230803', 's3', 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
 % Blackrock.thresholdSession('20230724', 's3', 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
 
-Blackrock.thresholdSession('20231212', subject_id, 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
+Blackrock.thresholdSession('20240112', subject_id, 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
 
 % Blackrock.thresholdSession('20230725', 's2', 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
 % Blackrock.thresholdSession('20230803', 's2', 'THRESHOLD', -4.5, 'noise_model', 'Blackrock')
@@ -25,7 +25,7 @@ Blackrock.thresholdSession('20231212', subject_id, 'THRESHOLD', -4.5, 'noise_mod
 
 tasktype = {};
 end_comment = {};
-session_dates = {'20231212'};
+session_dates = {'20240112'};
 taskfileTest = 2;
 
 idxToRemove = [];
